3afb8571ead3d781e1c7d83efb3db3c7eb3e1e80
Stefan Scharinger <master@scharez.at>
2024-02-28 21:31:38 +0100

    updated install script and typo in package.json

--- installscript.sh
+++ installscript.sh
@@ -3,9 +3,6 @@ echo -e "[SETUP] Install packages"
 apt-get update -qq > /dev/null 2>&1 && apt-get install -qq > /dev/null 2>&1 -y git wget
 cd /mnt/server
 
-echo -e "[SETUP] Create folders"
-mkdir tmp
-
 echo -e "[SETUP] Copy Github files"
 git clone https://github.com/scharez/ptero-directus-egg.git ./gtemp > /dev/null 2>&1
 
